50eb7bb1adf51c20dc03102c86395792f448aae9
yui-ubc <yui.mikuriya@gmail.com>
2025-04-19 13:37:15 -0700

    created a package

---
+++ R/my_function.R
@@ -0,0 +1,10 @@
+#' Split a string
+#'
+#' @param x A character vector with one element.
+#' @param split What to split on.
+#'
+#' @return A character vector.
+#' @export#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' strsplit1(x, split = ",")
